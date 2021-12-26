8e4fb1308589c243e5ecc3e1738878985290c332
Haaris Ghafoor <git@haaris.dev>
2021-12-26 18:31:39 -0500

    Add base setup

---
+++ blocking-api/src/main/java/dev/haaris/BlockingAPIApplication.java
@@ -0,0 +1,11 @@
+package dev.haaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockingAPIApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlockingAPIApplication.class, args);
+    }
+}
---
+++ non-blocking-api/src/main/java/dev/haaris/model/CustomResponse.java
@@ -0,0 +1,15 @@
+package dev.haaris.model;
+
+public class CustomResponse {
+    private String response;
+    public CustomResponse(final String response){
+        this.response = response;
+    }
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
